0020370afcdb31ec9c6b57e42019deef1f447e86
Kamil Wrobel <wrobelk@live.com>
2018-10-10 14:44:21 -0600

    creates tab bar, creates progile page, and inventory reference

---
+++ ProfileViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileViewController.swift
+//  Munchies
+//
+//  Created by Kamil Wrobel on 10/10/18.
+//  Copyright © 2018 Quinten Smith. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
