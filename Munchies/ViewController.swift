7b3a86e500ba57059b163e4b2b572abf549ed64b
QuintenSmith <quintensmith15@gmail.com>
2018-10-10 10:39:08 -0700

    Initial Commit

---
+++ Munchies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Munchies
+//
+//  Created by Quinten Smith on 10/8/18.
+//  Copyright © 2018 Quinten Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
