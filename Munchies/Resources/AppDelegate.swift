5b468403d694a019994524d2febd5c957e8ea308
QuintenSmith <quintensmith15@gmail.com>
2018-10-14 20:15:46 -0700

    Added SWRevealViewController to make sideMenu

--- Munchies/Resources/AppDelegate.swift
+++ Munchies/Resources/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-//        window = UIWindow()
-//        window?.rootViewController = MainTabBarController()
+      
+      //window?.rootViewController = MainTabBarController()
         return true
     }
 
